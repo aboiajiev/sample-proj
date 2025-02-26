36f627f26d18b6678452b5a72c7dd487b2d874be
lycce <aboiajiev@gmail.com>
2025-02-26 19:44:58 +0200

    Add git ignore

--- db/database.js
+++ db/database.js
@@ -1,4 +1,4 @@
-const config = require('./knexfile');
+const config = require('../knexfile');
 const knex = require('knex')(config.development);
 
 // TODO: Custom methods to handle different database operations can be developed here and be used across the application.
